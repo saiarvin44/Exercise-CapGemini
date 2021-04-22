eeb5b6e00595e8a8d91cb425c020098b04b79db8
saiarvin44 <saiarvind44@gmail.com>
2021-04-22 12:09:04 +0530

    feat: controller test

--- src/main/java/com/test/extendaRetail/ExtendaRetailTest/PerfectNumberController.java
+++ src/main/java/com/test/extendaRetail/ExtendaRetailTest/PerfectNumberController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/perfect")
 public class PerfectNumberController {
 
   private static Logger logger = LoggerFactory.getLogger(PerfectNumberController.class);
---
+++ src/test/java/com/test/extendaRetail/ExtendaRetailTest/ControllerTest.java
@@ -0,0 +1,29 @@
+package com.test.extendaRetail.ExtendaRetailTest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest()
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+public class ControllerTest {
+  @Autowired PerfectNumberService service;
+  @Autowired private MockMvc mvc;
+  private ObjectMapper mapper = new ObjectMapper();
+
+  @Test
+  public void isPerfect() throws Exception {
+
+    mvc.perform(get("/perfect/6")).andExpect(status().isOk());
+  }
+
+}
